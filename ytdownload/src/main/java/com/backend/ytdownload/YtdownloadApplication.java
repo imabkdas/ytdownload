193460bd06199aa6d4fe95c105ce8d205442abd0
imabkadas <anand.abhishek269@gmail.com>
2024-08-18 12:57:52 +0530

    sample project generated

---
+++ ytdownload/src/main/java/com/backend/ytdownload/YtdownloadApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ytdownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtdownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtdownloadApplication.class, args);
+	}
+
+}
---
+++ ytdownload/src/test/java/com/backend/ytdownload/YtdownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ytdownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtdownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
