4bf4a82eb386e9a46c77e3fafeda74d73be77a1a
imabkdas <anand.abhishek269@gmail.com>
2025-07-02 16:17:27 +0530

    code testing

--- frontend/src/components/DownloadFile.js
+++ frontend/src/components/DownloadFile.js
@@ -13,7 +13,7 @@ function DownloadFile() {
             if(quality === "mp3"){
                 const response = await axios.post("http://localhost:8080/api/audio/download", null, {
                     params: { url, format},
-                    responseType: 'blob' // Important to handle the downloads
+                    responseType: 'blob' // Important to handle the downloadss
                 });
     
                 const blob = new Blob([response.data], { type: response.headers['content-type'] });
